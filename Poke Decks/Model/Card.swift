69d724031ce57a9083fceb7ba7415c0a9d13acaf
TranceAerospace <xp46290@gmail.com>
2021-01-14 17:54:00 -0500

    Added default models

---
+++ Poke Decks/Model/Card.swift
@@ -0,0 +1,12 @@
+//
+//  Card.swift
+//  Poke Decks
+//
+//  Created by Mark Trance on 1/14/21.
+//
+
+import Foundation
+
+struct Card {
+	
+}
---
+++ Poke Decks/Model/Set.swift
@@ -0,0 +1,12 @@
+//
+//  Set.swift
+//  Poke Decks
+//
+//  Created by Mark Trance on 1/14/21.
+//
+
+import Foundation
+
+struct Set {
+	
+}
