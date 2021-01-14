667ab1dcb792e18cf6e41f8d8374cf601e860ace
TranceAerospace <xp46290@gmail.com>
2021-01-13 21:19:59 -0500

    Update CardSearchVC.swift

--- Poke Decks/CardSearchVC.swift
+++ Poke Decks/CardSearchVC.swift
@@ -8,22 +8,94 @@
 import UIKit
 
 class CardSearchVC: UIViewController {
+		
+		let logoImageView = UIImageView()
+		let usernameTextField = PKTextField()
+		let callToActionButton = PKButton(backgroundColor: .systemGreen, title: "Get Followers")
+		
+		var isUserNameEntered: Bool {
+			return !usernameTextField.text!.isEmpty
+		}
+		
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+		override func viewDidLoad() {
+			super.viewDidLoad()
 
-		view.backgroundColor = UIColor(red: 227/255, green: 86/255, blue: 39/255, alpha: 1.0)
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+			view.backgroundColor = .systemBackground
+			configureLogoImageView()
+			configureTextField()
+			configureCallToActionButton()
+			createDismissKeyboardTapGesture()
+		}
+		
+		override func viewWillAppear(_ animated: Bool) {
+			super.viewWillAppear(animated)
+			navigationController?.setNavigationBarHidden(true, animated: true)
+		}
+		
+		func createDismissKeyboardTapGesture() {
+			let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+			view.addGestureRecognizer(tap)
+			
+		}
+		
+		@objc func pushFollowerListVC() {
+			guard isUserNameEntered else {
+				presentGFAlertOnMainThread(title: "Empty Username", message: "Please Enter a Username. We need to know who to look for 😀.", buttonTitle: "OK")
+				return
+			}
+			
+			let cardListVC = CardListVC()
+			cardListVC.cardName = usernameTextField.text
+			cardListVC.title = usernameTextField.text
+			navigationController?.pushViewController(cardListVC, animated: true)
+		}
+		
+		
+		
+		
+		func configureLogoImageView() {
+			view.addSubview(logoImageView)
+			logoImageView.translatesAutoresizingMaskIntoConstraints = false
+			logoImageView.image = UIImage(named: "gh-logo")!
+			
+			NSLayoutConstraint.activate([
+				logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+				logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+				logoImageView.heightAnchor.constraint(equalToConstant: 200),
+				logoImageView.widthAnchor.constraint(equalToConstant: 200)
+			])
+		}
+		
+		func configureTextField() {
+			view.addSubview(usernameTextField)
+			usernameTextField.delegate = self
+			
+			NSLayoutConstraint.activate([
+				usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+				usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+				usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+				usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+			])
+		}
+		
+		func configureCallToActionButton() {
+			view.addSubview(callToActionButton)
+			callToActionButton.addTarget(self, action: #selector(pushFollowerListVC), for: .touchUpInside)
+			
+			NSLayoutConstraint.activate([
+				callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+				callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+				callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+				callToActionButton.heightAnchor.constraint(equalToConstant: 50)
+			])
+		}
 }
+
+	extension CardSearchVC: UITextFieldDelegate {
+		func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+			pushFollowerListVC()
+			return true
+		}
+	}
+
