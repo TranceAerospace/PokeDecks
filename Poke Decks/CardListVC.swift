cb57d43601ac522a4b243b5e8fed9461f3a8a17d
TranceAerospace <xp46290@gmail.com>
2021-01-13 21:19:53 -0500

    Create CardListVC.swift

---
+++ Poke Decks/CardListVC.swift
@@ -0,0 +1,31 @@
+//
+//  CardListVC.swift
+//  Poke Decks
+//
+//  Created by Mark Trance on 1/13/21.
+//
+
+import UIKit
+
+class CardListVC: UIViewController {
+	
+	var cardName: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
