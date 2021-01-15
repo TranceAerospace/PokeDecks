93a939ab051955d51d8788429c5a0ec80d8a84a1
TranceAerospace <xp46290@gmail.com>
2021-01-15 06:22:36 -0500

    Created placeholderr error handling

---
+++ Poke Decks/PKError.swift
@@ -0,0 +1,17 @@
+//
+//  PKError.swift
+//  Poke Decks
+//
+//  Created by Mark Trance on 1/14/21.
+//
+
+import Foundation
+
+enum PKError: String, Error {
+	case invalidUserName = "example"
+	case unableToComplete = "examples"
+	case invalidResponse = "example text"
+	case invalidData = "test"
+	
+	
+}
