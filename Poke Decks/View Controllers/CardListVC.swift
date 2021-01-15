7fbecd988fb9e94a0616495a393627852e355a4a
TranceAerospace <xp46290@gmail.com>
2021-01-15 06:24:42 -0500

    Added VC logic to show basic data

--- Poke Decks/View Controllers/CardListVC.swift
+++ Poke Decks/View Controllers/CardListVC.swift
@@ -9,23 +9,27 @@ import UIKit
 
 class CardListVC: UIViewController {
 	
-	var cardName: String!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	var setList: String!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .systemBackground
+		navigationController?.navigationBar.prefersLargeTitles = true
+		
+		NetworkManager.shared.getCards(for: setList, page: 1) { (result) in
+			
+			switch result {
+			case .success(let setList):
+				print(setList)
+			case .failure(let error):
+				self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "OK")
+			}
+		}
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		navigationController?.setNavigationBarHidden(false, animated: true)
+	}
 
 }
