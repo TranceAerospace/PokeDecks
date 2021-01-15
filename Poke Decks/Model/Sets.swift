326e911afa9ecab5a1e5a56569116f6293f355ff
TranceAerospace <xp46290@gmail.com>
2021-01-15 06:23:40 -0500

    Create Sets.swift

---
+++ Poke Decks/Model/Sets.swift
@@ -0,0 +1,14 @@
+//
+//  Set.swift
+//  Poke Decks
+//
+//  Created by Mark Trance on 1/14/21.
+//
+
+import Foundation
+
+struct Sets: Codable {
+	let name: String
+	let code: String
+	let totalCards: Int
+}
