f9a8275cc897dfecebac27026bf734943db87192
TranceAerospace <xp46290@gmail.com>
2021-01-17 08:34:31 -0500

    Refactored flow layout of collection view

--- Poke Decks/View Controllers/CardListVC.swift
+++ Poke Decks/View Controllers/CardListVC.swift
@@ -37,33 +37,16 @@ class CardListVC: UIViewController {
 	func configureViewController() {
 		view.backgroundColor = .systemBackground
 		navigationController?.navigationBar.prefersLargeTitles = true
-		
-		
 	}
 
 	func configureCollectionView() {
-		collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+		collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
 		view.addSubview(collectionView)
 		
 		collectionView.backgroundColor = .systemBackground
 		collectionView.register(CardCell.self, forCellWithReuseIdentifier: CardCell.reuseID)
 	}
-	
-	func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
-		let width = view.bounds.width
-		let padding: CGFloat = 12
-		let minimumItemSpacing: CGFloat = 10
 		
-		let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
-		let itemWidth = availableWidth / 3
-		
-		let flowLayout = UICollectionViewFlowLayout()
-		flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-		flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-		
-		return flowLayout
-	}
-	
 	func getFollowers() {
 		NetworkManager.shared.getCards(for: cardName, page: 1) { (result) in
 			
